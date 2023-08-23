c996945ff1246a288c927d3267e9f997274125ad
Darriel2603 <darrielmarkerizal@gmail.com>
2023-08-23 13:30:49 +0700

    tugas login page selesai

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -2,25 +2,36 @@ package com.example.hellotoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.hellotoast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        with(binding){
-            txtNumber.text = number.toString()
+        setContentView(R.layout.activity_main)
 
-            btnCount.setOnClickListener{
-                number++
-                txtNumber.text = number.toString()
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.buttonLogin)
+
+        val correctUsername = "darrielrel"
+        val correctPassword = "darriel1234"
+
+        loginButton.setOnClickListener {
+            val enteredUsername = usernameEditText.text.toString()
+            val enteredPassword = passwordEditText.text.toString()
+
+            if (enteredUsername == correctUsername && enteredPassword == correctPassword) {
+                showToast("Login Sukses")
             }
-            btnToast.setOnClickListener {
-                Toast.makeText(this@MainActivity, "Last count: $number", Toast.LENGTH_SHORT).show() }
+            else {showToast("Login Gagal")}
         }
+
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
